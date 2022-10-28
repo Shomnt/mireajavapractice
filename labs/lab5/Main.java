55a4701c5876059378ceb3ca8eca23c292691c2f
Shomnt <lesha.pashkov.00@mail.ru>
2022-10-29 00:31:37 +0300

    lab5

--- labs/lab5/Main.java
+++ labs/lab5/Main.java
@@ -5,20 +5,14 @@ import javax.swing.*;
 
 public class Main {
     public static void main (String[] args) {
-        Main main = new Main();
-        main.go();
-    }
 
-    public void go () {
         JFrame frame = new JFrame();
-        MyDrawPanel panel = new MyDrawPanel();
-
-        frame.getContentPane().add(panel);
-
         frame.setSize(300, 300);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new JLabel(new ImageIcon("image/cat.png")));
+        frame.add(new JLabel(new ImageIcon(args[0])));
         frame.show();
     }
+
+
 }
